368288bd7b56b41ce1a69e7bba93639ae3fe9849
Malik Soukri <malik.soukri@gmail.com>
2020-12-11 17:07:20 +0100

    profil_page_show_and_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :create_profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,4 +9,14 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :messages, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+
+  def create_profile
+    @profile = Profile.new
+    @user = User.last
+    @profile.user_id = @user.id
+    @profile.save
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :matches, only: [:index, :show ] do
     resources :bars, only: [:index]
   end
+
+  resources :profiles, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
